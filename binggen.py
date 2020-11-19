3820cfa0e2072f2327a84cbc05cbf17509760b73
marco7years <marcosettanni08@gmail.com>
2020-11-19 20:09:43 +0100

    browser file changed

--- binggen.py
+++ binggen.py
@@ -4,13 +4,10 @@ import os
 
 url = 'https://www.bing.com/search?q=reddit'
 
-webbrowser.register('edge', None, webbrowser.GenericBrowser(os.environ['ProgramFiles(x86)'] + r'\Microsoft\Edge\Application\msedge.exe'), preferred=True)
+webbrowser.register('edge', None, webbrowser.GenericBrowser(os.environ['ProgramFiles(x86)'] + r'\Microsoft\Edge\Application\msedge_proxy.exe'), preferred=True)
 webbrowser.open_new(url)
 
-fh = open('binggenlist.txt', 'r')
-for line in fh:
-
-    webbrowser.open(url+'+'+line)
-    time.sleep(2)
-
-fh.close()
+with open('binggenlist.txt', 'r') as fh:
+    for line in fh:
+        webbrowser.open(url+'+'+line)
+        time.sleep(2)
